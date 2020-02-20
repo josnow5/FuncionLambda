adffa0da12107c6c862d1d44dd70749792edb4ff
Josue Nuñez Prada <37277791+josnow5@users.noreply.github.com>
2020-02-19 20:54:33 -0500

    Add files via upload

---
+++ ejemploLambda.py
@@ -0,0 +1,18 @@
+def areaTriangulo(base, altura):
+	return (base*altura)/2
+
+print(areaTriangulo(4, 3))
+
+areaTrianguloLambda = lambda base, altura: (base*altura)/2
+
+print(areaTrianguloLambda(4, 3))
+
+def doblar(num):
+    resultado = num*2
+    return resultado
+
+doblar(2)
+
+doblar1 = lambda num: num*2
+
+doblar1(2)
